041d9973a41630cd65667918d60e20bb99f49ea3
Wiktor Gerlik <wiktorgerlik@gmail.com>
2022-12-03 21:06:20 +0100

    3gru

--- react-movies/src/components/sections/App/App.js
+++ react-movies/src/components/sections/App/App.js
@@ -1,51 +1,31 @@
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-
-
 import './App.css';
-
-// const baseFilms = [
-//   {
-//     Title: "Avatar",
-//     Year: "2009",
-//     Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
-//     imdbID: "tt0499549",
-//   },
-//   {
-//     Title: "I Am Legend",
-//     Year: "2007",
-//     Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI0NTI4NjE3NV5BMl5BanBnXkFtZTYwMDA0Nzc4._V1_.jpg",
-//     imdbID: "tt0480249",
-//   },
-//   {
-//     Title: "300",
-//     Year: "2006",
-//     Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMwNTg5MzMwMV5BMl5BanBnXkFtZTcwMzA2NTIyMw@@._V1_SX1777_CR0,0,1777,937_AL_.jpg",
-//     imdbID: "tt0416449",
-//   }
-// ]
-
 
 function App() {
 
   const [searchInputValue, setSearchInputValue] = useState('');
-  const [movies, setMovies] = useState([]);
   const [addTitleInputValue, setAddTitleInputValue] = useState('');
   const [addYearInputValue, setAddYearInputValue] = useState('');
   const [addSrcInputValue, setAddSrcInputValue] = useState('');
+
+  const [movies, setMovies] = useState([]);
+  //pomocniczy state do przetrzymywania zfiltrowanych filmów:
+  const [fMovies, setFMovies] = useState([]);
 
   useEffect(()=>{
     fetch('http://localhost:5000/movies')
       .then((res)=>res.json())
       .then(data=>{
-        setMovies(data)})
+        setMovies(data)
+        setFMovies(data)
+      })
 
     // const moviesFromLS = localStorage.getItem('moviesLS');
     // if(moviesFromLS){setMovies(JSON.parse(moviesFromLS))}
     // else{moviesFromLS = localStorage.setItem('moviesLS', JSON.stringify(movies));}
   }, []);
-//na razie nie filtruje ponownie, tlko zmienia tablice na odfiltrowane!
 
   const handleFilter = (event) => {
     event.preventDefault();
@@ -54,7 +34,7 @@ function App() {
           return movie.Title.toLowerCase().includes(searchInputValue.toLowerCase())
       });
     //const newMoviesArray = films.concat(filteredMovies);
-    setMovies(filteredMovies);
+    setFMovies(filteredMovies);
     setSearchInputValue('');
   }
 
@@ -80,10 +60,10 @@ function App() {
       Title: addTitleInputValue,
       Year: addYearInputValue,
       Poster: addSrcInputValue,
-      imdbID: uuidv4()
+      id: uuidv4()
     }
     const newMoviesArray = movies.concat(newMovie);
-    setMovies(newMoviesArray);
+    
     fetch('http://localhost:5000/movies/',{
       method: 'POST',
       headers: {
@@ -91,6 +71,8 @@ function App() {
       },
       body: JSON.stringify(newMovie)
     })
+    setFMovies(newMoviesArray);
+    setMovies(newMoviesArray);
 
     //localStorage.setItem('moviesLS', JSON.stringify(newMoviesArray));
     setAddTitleInputValue('');
@@ -100,14 +82,36 @@ function App() {
 
   const handleRemoveMovie = (idToRemove) => {
     const moviesWithRemovedItem = movies.filter(movie=>{
-      return movie.imdbID !== idToRemove
+      return movie.id !== idToRemove
     })
-    setMovies(moviesWithRemovedItem);
     fetch(`http://localhost:5000/movies/${idToRemove}`, {
       method: 'DELETE'
     })
-
+    setFMovies(moviesWithRemovedItem);
+    setMovies(moviesWithRemovedItem);
     // localStorage.setItem('moviesLS', JSON.stringify(moviesWithRemovedItem));
+  }
+
+  const renderMovies = (renderedMovies) => {
+              return (renderedMovies
+                // .filter(movie => {
+                //   return movie.Title.toLowerCase().includes(searchInputValue.toLowerCase())
+                // })
+                    .map(movie => {
+                      return (
+                        <div key={movie.id} className="container">
+                          <div>
+                              <img className="movie__poster" src={movie.Poster}/>
+                          </div>
+                          <div className="container_columns">
+                              <p className="description">TYTUŁ:<br/> {movie.Title}</p>
+                              
+                              <p className="description">ROK:<br/> {movie.Year}</p>
+                              <button onClick={()=>handleRemoveMovie(movie.id)}>remove</button>
+                          </div>  
+                        </div>
+                      )
+              }))
   }
   
   return (
@@ -156,25 +160,8 @@ function App() {
   
           <div id="list" className="list" type="list">
             
-            {movies
-      //       .filter(movie => {
-      //     return movie.Title.toLowerCase().includes(searchInputValue.toLowerCase())
-      // })
-      .map(movie => {
-              return (
-                <div key={movie.imdbID} className="container">
-                  <div>
-                      <img className="movie__poster" src={movie.Poster}/>
-                  </div>
-                  <div className="container_columns">
-                      <p className="description">TYTUŁ:<br/> {movie.Title}</p>
-                      
-                      <p className="description">ROK:<br/> {movie.Year}</p>
-                      <button onClick={()=>handleRemoveMovie(movie.imdbID)}>remove</button>
-                  </div>  
-                </div>
-              )
-            })}
+            {renderMovies(fMovies)
+            }
             
           </div>
   
